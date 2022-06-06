791d59df9b8baa7fee73aee4834d6c2ebcd390bd
profjaquetafine <88409934+profjaquetafine@users.noreply.github.com>
2022-06-06 09:46:21 -0300

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,31 +1,4 @@
 class Player{
-constructor(){}
-getCount(){
-    var playerCountRef = database.ref("playerCount")
-    playerCountRef.on("value",function(data){
-    
-        playerCountRef=data.val();
-     
-    })
-
-}
-    
-    updateCount(count){
-database.ref('/').update({
-
-playerCount:count
-
-});
-
-    }
-update(name){
-
-    var playerIndex="player"+ playerCount;
-    database.ref(playerIndex).set({
-name:name
-
-    })
-}
 
 
 
